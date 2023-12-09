c832599f988a133f1ea7e82e39d89227b473bbe9
psychedelickoala <77258902+psychedelickoala@users.noreply.github.com>
2023-12-09 15:31:40 +1100

    implementing outlier search

--- data_analysis_v3.py
+++ data_analysis_v3.py
@@ -3,20 +3,24 @@ import numpy as np
 
 class StatsCalculator:
     def __init__(self, data: str | np.ndarray) -> None:
+        print("initialising")
         self.__filename = None
         self.__attribute_names = []
-        self.__data = None
-        self.__covariance = None
+        self.__mahal_dists = None
+
         if type(data) == str:
-            self.reset(data)
+            self.read_from_file(data)
         else:
             self.__data = data
+        self.__dim = self.__data.shape[0]
+        self.sort_data()
+        
         self.__covariance = np.cov(self.__data)
-        self.__B = np.linalg.cholesky(self.__covariance)  # B @ B.T = covariance
-        self.__sorted_data = self.sort_data()
+        self.__covariance_inv = np.linalg.inv(self.__covariance)
+        self.__from_base = np.linalg.cholesky(self.__covariance)  # B @ B.T = covariance
+        self.__to_base = np.linalg.inv(self.__from_base)
 
-        avging_vec = np.ones((self.__data.shape[1], 1))/self.__data.shape[1]
-        self.__mean = self.__data @ avging_vec
+        self.__mean = np.mean(data, axis = 1)
 
 
     def __len__(self) -> int:
@@ -25,16 +29,48 @@ class StatsCalculator:
     def get_covariance(self) -> np.ndarray[np.ndarray[float]]:
         return self.__covariance
     
+    def get_data(self) -> np.ndarray:
+        return self.__data
+    
+    def get_mean(self) -> np.ndarray:
+        return self.__mean
+    
     def get_attribute_names(self) -> list[str]:
         return self.__attribute_names
     
-    def sort_data(self) -> np.ndarray:
-        t_data = np.linalg.inv(self.__B) @ self.__data
+    def m_dist(self, point: np.ndarray) -> float:
+        return np.sqrt((point - self.__mean.T) @ self.__covariance_inv @ (point.T - self.__mean))
+    
+    def sort_data(self) -> None:
+        covariance = np.cov(self.__data)
+        basis = np.linalg.cholesky(covariance)
+        mean = np.mean(self.__data, axis = 1, keepdims=True)
+
+        t_data = np.linalg.inv(basis) @ (self.__data - mean)
         indexlist = np.argsort(np.linalg.norm(t_data, axis=0))
         sorted_t_data = t_data[:, indexlist]
-        return self.__B @ sorted_t_data
 
-    def reset(self, filename) -> None:
+        self.__mahal_dists = np.linalg.norm(sorted_t_data, axis=0)
+        self.__data = (basis @ sorted_t_data) + mean
+    
+    def get_outlier_plane(self, cutoff: float) -> tuple[np.ndarray]:
+        ind = np.searchsorted(self.__mahal_dists, cutoff)
+        print(f"ind: {ind}")
+        t_outliers = (self.__to_base @ self.__data).T[ind:]
+        if t_outliers.shape[0] == 0:
+            raise ValueError("No data points outside confidence interval")
+        elif t_outliers.shape[0] == 1:
+            u = (self.__from_base @ t_outliers[0]) - self.__mean
+            e1 = np.zeros(self.__dim); e1[0] = 1.0
+            e2 = np.zeros(self.__dim); e2[1] = 1.0
+            v = u + (e2 if np.allclose(u/np.linalg.norm(u), e1) else e1)
+            return u, v
+        t_outliers -= np.mean(t_outliers, axis = 0, keepdims=True)
+        t_proj = np.linalg.svd(t_outliers, full_matrices=True).Vh
+        return self.__from_base @ t_proj[0], self.__from_base @ t_proj[1]
+
+
+    def read_from_file(self, filename) -> None:
         self.__filename = filename
         with open(self.__filename) as data_file:
             csv_reader = csv.reader(data_file, delimiter=',')
@@ -52,12 +88,6 @@ class StatsCalculator:
 
         self.__data = self.__data.T
 
-    def get_outliers(self, cutoff: float) -> list[np.ndarray]:
-        outliers = []
-        for point in self.__data.T:
-            m_dist = np.sqrt((point - self.__mean.T) @ np.linalg.inv(self.__covariance) @ (point.T - self.__mean))
-            if m_dist > cutoff:
-                outliers.append(point)
-        return outliers
+
 
     
--- testing_v3.py
+++ testing_v3.py
@@ -5,11 +5,26 @@ from time import time
 
 
 dim = 3
-num_data = 5
+num_data = 1000
 
-data = np.random.rand(dim, num_data) - 0.5
-avging_vec = np.ones((num_data, 1))/num_data
+data = np.random.chisquare(4, size = (dim, num_data))
+
+#cov_base = np.random.rand(dim, dim)
+#cov = cov_base @ cov_base.T
+#centre = np.random.rand(dim)
+
+#data = np.random.multivariate_normal(centre, cov, size=num_data)
 
 SC = StatsCalculator(data)
+u, v = SC.get_outlier_plane(3)
 
+A = np.linalg.inv(SC.get_covariance())
+EC = EllipseCalculator(A, num_points=30)
+
+P = EC.get_projection_matrix(u, v)
+proj_data = EC.points_onto_plane(P, SC.get_data())
+axes = EC.axes_onto_plane(P)
+ellipses = EC.ellipsoid_onto_plane(P, SC.get_mean(), m_dists = [1, 2, 3])
+
+EC.plot_on_plane(ellipses, proj_data, axes)
 
